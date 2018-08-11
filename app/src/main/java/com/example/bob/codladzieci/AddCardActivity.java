1eac3f28327f3b3cb287b1e91ea419dc8db5f2a7
Robert <robbooda@gmail.com>
2018-08-11 14:35:48 +0200

    + correct Firebase implementation + FirebaseDatabase

--- app/src/main/java/com/example/bob/codladzieci/AddCardActivity.java
+++ app/src/main/java/com/example/bob/codladzieci/AddCardActivity.java
@@ -4,13 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AddCardActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mCardDatabaseReference;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
 
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mCardDatabaseReference = mFirebaseDatabase.getReference().child("cards");
     }
 
     @Override
